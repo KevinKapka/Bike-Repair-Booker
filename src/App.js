c5953e1d9d0f91623d32cc44ebf55fa76bcfc0e8
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-11-18 17:35:18 -0600

    splash layout in Landing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router-dom";
 
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Landing.scss";
+import logo from '../../svgs/logo.svg'
 
 function Landing() {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div id="Landing">
+      <div id="splash">
+        <div id="splash-img-container">
+          <img src="" alt="splash-img" id="splash-img" />
+        </div>
+        
+        <div className='splash-logo-container'>
+          <img src={logo} alt="site logo. Houston CycleFix" id="splash-logo" />
+          <h1>Mobile Bicycle Repair</h1>
+        </div>
+        <div className='splash-button-container'>
+          <button id='book-now'>Book Now</button>
+          <button id='how-it-works'>How it Works</button>
+        </div>
+      </div>
+
     </div>
   );
 }
