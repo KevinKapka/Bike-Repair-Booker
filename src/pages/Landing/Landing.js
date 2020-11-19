5b5757ee22eab64e6d182dca975816d0af97780a
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-11-19 09:23:55 -0600

    landing out for splash and how to done

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -5,21 +5,39 @@ import logo from '../../svgs/logo.svg'
 function Landing() {
   return (
     <div id="Landing">
-      <div id="splash">
-        <div id="splash-img-container">
+      <div className="splash-container">
+        <div className="splash-img-container">
           <img src="" alt="splash-img" id="splash-img" />
-        </div>
-        
+        </div>  
         <div className='splash-logo-container'>
           <img src={logo} alt="site logo. Houston CycleFix" id="splash-logo" />
           <h1>Mobile Bicycle Repair</h1>
         </div>
         <div className='splash-button-container'>
-          <button id='book-now'>Book Now</button>
-          <button id='how-it-works'>How it Works</button>
+          <button className='landing-btn-1'>Book Now</button>
+          <button className='landing-btn-2'>How it Works</button>
         </div>
       </div>
 
+        <div className='info-container'>
+          <h1>HOW IT WORKS</h1>
+          <ol>
+            <li>
+              <h2>1. BOOK AN APPOINTMENT</h2>
+              <p>Whether you need a tune-up or a new-in-box bicycle built, book an appointment and we will work around your busy schedule.</p>
+            </li>
+            <li>
+              <h2>2. REPAIR WITHOUT STRESS</h2>
+              <p>Whether you need a tune-up or a new-in-box bicycle built, book an appointment and we will work around your busy schedule.</p>
+            </li>
+            <li>
+              <h2>3. GET ROLLING AGAIN</h2>
+              <p>Whether you need a tune-up or a new-in-box bicycle built, book an appointment and we will work around your busy schedule.</p>
+            </li>
+          </ol>
+          <button className='landing-btn-1'>Book Now</button>
+          <button className='landing-btn-2'>Contact Us</button>
+        </div>
     </div>
   );
 }
