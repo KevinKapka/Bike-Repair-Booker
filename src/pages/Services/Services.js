5885234e1e06f4c8255754e0ddae3c6864b60c52
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-11-19 14:15:07 -0600

    Created Service Cards

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,9 +1,44 @@
 import React from "react";
+import './Services.scss';
+import { servicesCopy } from './servicesData';
+
+const servicesCards = servicesCopy.map(service => {
+  if(service.header !== 'ADDITIONAL SERVICES'){
+    return(
+      <div key={service.header} className='serviceCard'>
+        <div className='header-container'>
+          <h1 className='header'>{service.header}</h1>
+          <h1 className='price'>{service.price}</h1>
+        </div>
+        <h2 className='title'>{service.title}</h2>
+        <p className='body'>{service.discription}</p>
+        <button>Book Now</button>
+      </div>
+    )
+  }else {
+    return(
+      <div key={service.header} className='serviceCard'>
+        <div className='header-container'>
+          <h1 className='header'>{service.header}</h1>
+        </div>
+        <button>Book Now</button>
+      </div>
+    )
+  }
+
+})
 
 function Services() {
+
   return (
-    <div>
-      <h2>Services</h2>
+    <div className='Services'>
+      <h1>Services</h1>
+      <p>Prices may vary based on complexity. Pricing does not include parts or any necessary additional materials.</p>
+      <div className='card-container'>
+        {servicesCards}
+      </div>
+
+
     </div>
   );
 }
---
+++ src/pages/Services/servicesData.js
@@ -0,0 +1,45 @@
+export const servicesCopy = [
+  {
+    header: 'BASIC TUNE UP',
+    price: '$64.99',
+    title: 'Basic Tune Up Includes:',
+    discription: 'Full Safety Inspection. Inspect, clean and adjust all shifters, cables and derailleurs. Inspect clean and adjust brake levers, brakes, and cables. Inspect, clean, and lubricate drivetrain on bike. Inspect all headset, crankset, and pulley, and wheel bearings. Check and adjust all bolt torque’s to proper adjustment. Basic Cleaning.',
+
+  },
+  {    
+    header: 'INTERMEDIATE TUNE UP',
+    price: '$94.99',
+    title: 'Intermediate Tune Up Includes:',
+    discription: 'Full Safety Inspection. Inspect, clean and adjust all shifters, cables and derailleurs. Inspect clean and adjust brake levers, brakes, and cables. Inspect all headset, crankset, and pulley, and wheel bearings. Check and adjust all bolt torque’s to proper adjustment. Basic Cleaning. Inspect, clean, and lubricate drivetrain off bike. Replace all shifting and brake cables if necessary. True wheels. ',
+  },
+  {    
+    header: 'FULL TUNE UP',
+    price: '$174.99',
+    title: 'Full Tune Up Includes:',
+    discription: 'Full Safety Inspection. Inspect, clean and adjust all shifters, cables and derailleurs. Inspect clean and adjust brake levers, brakes, and cables. Check and adjust all bolt torque’s to proper adjustment. Basic Cleaning. Inspect, clean, and lubricate drivetrain off bike. Replace all shifting and brake cables if necessary. True wheels. Inspect all headset, crankset, and pulley, and wheel bearings. '
+  },
+  {    
+    header: 'ADDITIONAL SERVICES',
+    price: '$174.99',
+    1: {
+      name: 'Build New Bike From Box',
+      price: '$59.99'
+    },
+    2: {
+      name: 'Build Frame Up',
+      price: 'Contact Us'
+    },
+    3: {
+      name: 'Bleed Hydraulic Brakes',
+      price: '$44.99'
+    },
+    4: {
+      name: 'Suspension Adjustment',
+      price: '$44.99'
+    },
+    5: {
+      name: 'Multi-Bike-Discount',
+      price: 'Contact Us'
+    }
+  }
+]
