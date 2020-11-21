3a0692cb1002c199afa8fd0fad9a810c66dab4ca
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-11-21 12:10:42 -0600

    smooth scroll on calendar

--- src/pages/Booking/Calendar.js
+++ src/pages/Booking/Calendar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 import './Calendar.scss'
@@ -6,6 +6,14 @@ import backButton from '../../svgs/backButton.svg';
 import Calender from "../../components/Calendar";
 
 function Calendar({input}) {
+    useEffect(()=>{
+        window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+    }, [])
+
     return (
         <div className='BookCalendar'>
             <div className='page-header'>
