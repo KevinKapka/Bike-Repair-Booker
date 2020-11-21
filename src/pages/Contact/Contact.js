6d0afbbaae922676421d9eaa200678ec15f50d29
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-11-21 11:05:12 -0600

    ContactUS works!

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import { Form, Button } from "react-bootstrap";
+import emailjs from 'emailjs-com'
 
 import './Contact.scss'
 
 function Contact() {
+
+    function sendEmail(e){
+        e.preventDefault();
+        console.log('e.target', e.target)
+        emailjs.sendForm('service_5qhb5bm', 'template_g3rbt0j', e.target, 'user_1tTUVlfVHJ0QgAOqETSmf')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+    }
+
+
     return (
         <div className='Contact'>
             <div className='page-header'>
@@ -12,32 +27,32 @@ function Contact() {
                 <p>We have the answers! Get in touch and we will respond within 24-48 hours.</p>      
             </div>
             <div className='page-body'>
-                <Form className='contact-form'>
+                <Form className='contact-form' onSubmit={sendEmail}>
                     <p><span>*</span>Required Field</p>
-                <Form.Group controlId="formBasicFirstName">
-                    <Form.Label>First name*</Form.Label>
-                    <Form.Control />
-                </Form.Group>
-                <Form.Group controlId="formBasicLastName">
-                    <Form.Label>Last name</Form.Label>
-                    <Form.Control  />
-                </Form.Group>                
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email*</Form.Label>
-                    <Form.Control type="email"  />
-                </Form.Group>
+                    <Form.Group controlId="formBasicFirstName">
+                        <Form.Label>First name*</Form.Label>
+                        <Form.Control name='firstName'/>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicLastName">
+                        <Form.Label>Last name</Form.Label>
+                        <Form.Control name='lastName' />
+                    </Form.Group>                
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email*</Form.Label>
+                        <Form.Control type="email"  name='email'/>
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicLastName">
-                    <Form.Label>Phone</Form.Label>
-                    <Form.Control  />
-                </Form.Group>
-                <Form.Group controlId="formBasicLastName">
-                    <Form.Label>How can we help*</Form.Label>
-                    <Form.Control as='textarea' rows={4} />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Send Message
-                </Button>
+                    <Form.Group controlId="formBasicPhone">
+                        <Form.Label>Phone</Form.Label>
+                        <Form.Control name='phone' />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicMessage">
+                        <Form.Label>How can we help*</Form.Label>
+                        <Form.Control as='textarea' rows={4} name='message'/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Send Message
+                    </Button>
                 </Form>
             </div>
         </div>
